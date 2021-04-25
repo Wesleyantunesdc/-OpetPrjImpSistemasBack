f99dd18262cb26336ec791726d34a7052c57e0ed
Wesleyantunesdc <wesleyantunesdca@gmail.com>
2021-04-25 16:57:57 -0300

    Back-end criado

---
+++ src/main/java/com/opet/implantacao/ImplantacaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.opet.implantacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplantacaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImplantacaoApiApplication.class, args);
+	}
+
+}
